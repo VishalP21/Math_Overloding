a2fbcb0d9accd85a663fc7f861dfbc6cdba36b85
Vishal Prajapati <vishalp951295@gmail.com>
2024-07-31 19:31:48 +0530

    Add project files.

---
+++ Math_Overloding/Program.cs
@@ -0,0 +1,7 @@
+﻿using Math_Overloding;
+
+example example =  new example();
+
+example.multi(10, 12);
+example.multi(10, 12, 8);
+example.multi(10.25, 12.30);
---
+++ Math_Overloding/example.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Overloding
+{
+    public class example
+    {
+        public void multi(int a,int b)
+        {
+            Console.WriteLine("muti of 2 num = " + a*b);
+        }
+        public void multi(int a, int b, int c)
+        {
+            Console.WriteLine("muti of 2 num = " + a * b * c);
+        }
+        public void multi(double a, double b)
+        {
+            Console.WriteLine("muti of 2 num = " + a * b);
+        }
+
+    }
+}
